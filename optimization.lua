3b847531cf78d8707bffb7afec977e6392130173
LoapZ <181424102+LoapCoderZ@users.noreply.github.com>
2026-07-30 03:29:45 +0100

    Create optimization.lua

---
+++ optimization.lua
@@ -0,0 +1,2 @@
+pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/LoapCoderZ/kohlslitev2/refs/heads/main/source.lua"))() end)
+pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/LoapCoderZ/kohlslitev2/refs/heads/main/speed.lua"))() end)
